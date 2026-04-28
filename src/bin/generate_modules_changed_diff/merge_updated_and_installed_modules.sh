796ee4eb124e0737145097c7d9d91ea827a24827
hparfr <raphael.reverdy@akretion.com>
2026-04-28 10:50:47 +0200

    fix: all updated modules were flagged as silent updates

--- src/bin/generate_modules_changed_diff/merge_updated_and_installed_modules.sh
+++ src/bin/generate_modules_changed_diff/merge_updated_and_installed_modules.sh
@@ -2,9 +2,14 @@
 # Some modules like core, custom modules, or patched module do not increment their version number
 
 # USAGE
-#    merge_updated_and_installed_modules.sh modules_after modules_updated
+#    merge_updated_and_installed_modules.sh modules_before modules_after modules_updated
 #
 # Example
+#  modules_before:
+#   base,18.0.1.0.0
+#   module_not_installed,
+#   module_1,18.0.1.0.1
+#   module_2,18.0.1.1.0
 #
 #  modules_after:
 #   base,18.0.1.0.0
@@ -17,10 +22,10 @@
 #   module_2
 #
 #  output:
-#   base,18.0.1.0.0**
+#   base,18.0.1.0.0* silent update
 #   module_not_installed,
 #   module_1,18.0.1.0.1
-#   module_2,18.0.2.0.0**
+#   module_2,18.0.2.0.0
 
 if [ -n "$1" ]; then
     # test if file is readable
@@ -36,18 +41,27 @@ if [ -n "$2" ]; then
         exit 1
     fi
 fi
-MODULES_UPDATED_FILE="$1"
+if [ -n "$3" ]; then
+    # test if file is readable
+    if [ ! -f "$3" ] || [ ! -r "$3" ]; then
+        echo "File '$3' do not exist or is unreadable." >&2
+        exit 1
+    fi
+fi
+MODULES_BEFORE="$1"
 MODULES_AFTER="$2"
+MODULES_UPDATED_FILE="$3"
 
 gawk -F, '
-    ARGIND == 1 { updated[$1]; next }
+    ARGIND == 1 { before[$1] = $2; next }
+    ARGIND == 2 { updated[$1]; next }
     {
         # $1 nom du module
         # $2 version du module
-        if ($1 in updated) {
+        if ($1 in updated && $2 == before[$1]) {
             print $1 "," $2 "* silent update"
         } else {
             print $1 "," $2
         }
     }
-' "${MODULES_UPDATED_FILE}" "${MODULES_AFTER}"
+' "${MODULES_BEFORE}" "${MODULES_UPDATED_FILE}" "${MODULES_AFTER}"
